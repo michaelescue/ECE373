93f2fea7a77f85bf811f2889006cb749bd9295f7
michaelescue <mescue@pdx.edu>
2019-06-09 23:18:41 -0700

    refresh and update

---
+++ ecd.sh
@@ -0,0 +1,9 @@
+cd /home/me/Documents/ece373/Assignments/HW5/sourcefiles
+echo DIRECTORY CHANGED TO:
+pwd
+echo
+sudo bash
+
+echo SUPERUSER ENGAGED
+echo
+exit
---
+++ refresh0.sh
@@ -0,0 +1,5 @@
+cd /home/me/Documents/ece373
+echo REFRESHING
+pwd
+./refresh.sh
+exit
